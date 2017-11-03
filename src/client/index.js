798aca4b579fe2668d49f33aad4ea2d48f7899ec
fuchi15 <sakuya815@gmail.com>
2017-11-03 23:51:49 +0900

    component article add

--- src/client/index.js
+++ src/client/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import Main from './components/main.jsx';
+import Main from './components/main.jsx'
 
 ReactDOM.render(
   <Main message='and GoodBye World' date={new Date()} />,
