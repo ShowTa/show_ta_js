5ed6b92847c5130c99ae6f2a9b1481710aabe446
fuchi15 <sakuya815@gmail.com>
2017-11-08 16:10:15 +0900

    記事投稿の入力をマークダウンに変更

--- src/client/components/article/form.js
+++ src/client/components/article/form.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
+import marked from 'marked'
 
 import axios from 'axios'
 
@@ -9,13 +10,14 @@ export default class ArticleForm extends Component {
     constructor(props){
         super(props);
         this.state = {
-            data: []
+            data: [],
+            text: 's'
         };
+    this.textInput = this.textInput.bind(this);
     }
-
     submitHandler(event) {
         let title = document.getElementById('title').value
-        let content = document.getElementById('content').value
+        let content = document.getElementById('textarea').value
         axios.post(REQUEST_URL + 'article/create', {
             title: title,
             content: content
@@ -23,15 +25,35 @@ export default class ArticleForm extends Component {
           .then(function (response) {
             console.log(response);
         });
+        console.log(title, content)
     }
-
+    converter() {
+        var convText = marked(this.state.text, {
+            sanitize: true
+        });
+        return {
+            __html: convText
+        };
+    }
+    textInput() {
+        this.setState({
+          text: document.getElementById('textarea').value
+        })
+    }
     render() {
         return(
             <div>
                 <p>記事名</p>
                 <input id="title" type="text"/>
                 <p>記事の内容</p>
-                <input id="content" type="text"/>
+                <div className="row">
+                    <div className="col-sm-6">
+                        <textarea id="textarea" className="text-field" onChange={this.textInput}>{this.state.text}</textarea>
+                    </div>
+                    <div className="col-sm-6">
+                        <span dangerouslySetInnerHTML={this.converter()} />
+                    </div> 
+                </div>
                 <button type="button" onClick={this.submitHandler}>登録する</button>
             </div>
         )
